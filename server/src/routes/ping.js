522299931a1139d8f897ed7edd2b7d1a09ec0cf6
vaskodegama <vasr161095@gmail.com>
2021-08-20 16:35:07 +0300

    ready user endpoints

--- server/src/routes/ping.js
+++ server/src/routes/ping.js
@@ -3,17 +3,22 @@
 const httpContext = require('express-http-context')
 const mongoose = require('mongoose')
 const express = require('express')
+const DTO = require('../models/DTO')
+const BaseController = require('../controllers/base-controller')
 
 const pingRouter = express.Router()
 
 pingRouter.get('', (req, res, next) => {
   const startTime = httpContext.get('reqStartTime')
-  res.status(200).json({
+  const dto = new DTO(req)
+  dto.data = {
     success: true,
     isOnline: true,
     timing: Date.now() - startTime,
     dbState: mongoose.STATES[mongoose.connection.readyState]
-  })
+  }
+
+  BaseController.setResponse({ req, res, dto })
   next()
 })
 
